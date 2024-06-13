8b78a873782653269e536653ba6d9081d8b0b710
Rafia Ahsan <160255825+Rafia-Ahsan@users.noreply.github.com>
2024-06-13 23:25:07 +0500

    main.ts

---
+++ Day-16/q47/main.js
@@ -0,0 +1,10 @@
+// QUESTION :47
+// Advanced Array Destructuring: Given an array of objects representing different laptops, each with properties make, model, and year, use array destructuring to assign the first and second laptops to variables. Then, log these variables.
+var laptops = [
+    { make: "DELL", model: "XS-05", year: 2022 },
+    { make: "HP", model: "Macbook Air", year: 2024 },
+    { make: "Lenovo", model: "LV-075", year: 2007 },
+];
+var laptop1 = laptops[0], laptop2 = laptops[1];
+console.log(laptop1);
+console.log(laptop2);
--- Day-16/q47/main.ts
+++ Day-16/q47/main.ts
@@ -1,1 +1,10 @@
-
+// QUESTION :47
+// Advanced Array Destructuring: Given an array of objects representing different laptops, each with properties make, model, and year, use array destructuring to assign the first and second laptops to variables. Then, log these variables.
+let laptops =[
+    {make :"DELL" , model:"XS-05" , year:2022},
+    {make:"HP" , model :"Macbook Air" , year:2024},
+    {make:"Lenovo" , model: "LV-075" , year :2007},
+];
+let [laptop1 , laptop2]=laptops;
+console.log(laptop1);
+console.log(laptop2);

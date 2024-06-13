393ac5c3baf350eab3c16e759b061dd8c0225a44
Rafia Ahsan <160255825+Rafia-Ahsan@users.noreply.github.com>
2024-06-13 23:27:37 +0500

    main.ts

---
+++ Day-17/q50/main.js
@@ -0,0 +1,4 @@
+// QUESTION: 50
+// Multiline Template Literals: Use template literals to create a multiline string that describes your ideal day. Include at least three different activities.
+var favActivities = "I love these activities\n1. love playing football.\n2.I love watching T.V.\n3.I love eating fastfood.";
+console.log("".concat(favActivities));
--- Day-17/q50/main.ts
+++ Day-17/q50/main.ts
@@ -1,1 +1,7 @@
-
+// QUESTION: 50
+// Multiline Template Literals: Use template literals to create a multiline string that describes your ideal day. Include at least three different activities.
+let favActivities :string =`I love these activities
+1. love playing football.
+2.I love watching T.V.
+3.I love eating fastfood.`
+console.log(`${favActivities}`);

edbdd8fe99e1a364209546cd102cb8d22d55b209
Rafia Ahsan <160255825+Rafia-Ahsan@users.noreply.github.com>
2024-06-13 23:29:00 +0500

    main.ts

--- Day-18/q52/main.ts
+++ Day-18/q52/main.ts
@@ -1,1 +1,12 @@
-
+// QUESTION :52
+// Make a Smartphone Object: Create a simple way to keep track of a smartphone's details. Include its brand, model, and other key features like how much storage it has, the size of its screen, and how long its battery lasts.
+let smartphone ={
+    make:"IPHONE",
+    model:"XSMAX",
+    specs:{
+        storage:"128gb",
+        screen:"12x34 inches",
+        batterylife:"15 hours"
+    }
+};
+console.log(smartphone);

dca5d402a782664e6e4d1968f7a37eae689c1501
Rafia Ahsan <160255825+Rafia-Ahsan@users.noreply.github.com>
2024-06-13 23:34:04 +0500

    main.ts

--- Day-20/q59/main.ts
+++ Day-20/q59/main.ts
@@ -1,1 +1,17 @@
+// QUESTION :59
+// Average Score Calculator: Write a simple program that can take lots of scores and find their average.
 
+// Explain & TIP: This program can handle any number of scores you give it, and it tells you the average score. Handy for seeing how well you did overall!
+
+// This program makes a function that adds a specific number
+function makeAdder(valueToAdd: number): (number) => number {
+    // This is the magic box. It takes a number and adds your special number to it
+    return function(number: number): number {
+        return number + valueToAdd;
+    };
+}
+
+// Making a magic box that adds 5
+let addFive = makeAdder(5);
+console.log(addFive(10)); // If we put 10 in the box, it gives us 15
+// We made a function (magic box) that adds 5 to any number.

1e2a4f3dcd4c0b162953577209a07e2977340e98
Rafia Ahsan <160255825+Rafia-Ahsan@users.noreply.github.com>
2024-06-22 01:57:01 +0500

    main.ts

--- Day-21/q61/main.ts
+++ Day-21/q61/main.ts
@@ -1,1 +1,14 @@
+// Question 61: Making Enums for Vehicles: Let's create categories for vehicles like cars, trucks, and motorcycles using enums, and show one example.
 
+// ENUM:It is a special type of lsts used to made code cleaner 
+
+enum vehicleType{
+    car,
+    motorcyle,
+    bicyle,
+    truck,
+    aeroplane,
+}
+console.log(vehicleType.truck); //here we are just checking the that at which index of vehicleType truck exists
+console.log(vehicleType.train); //It will give error b/c there's no as such vehice type in the enum.
+console.log(vehicleType.car);  //It will first check whether this exists in enum .if car exist then it will tell you the index of car in the form of output.

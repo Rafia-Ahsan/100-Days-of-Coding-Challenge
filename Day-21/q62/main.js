c2d3820c48bc271e39498ec2be741ded437f37c6
Rafia Ahsan <160255825+Rafia-Ahsan@users.noreply.github.com>
2024-06-22 02:10:50 +0500

    main.ts

---
+++ Day-21/q62/main.js
@@ -0,0 +1,9 @@
+// Question 62: Making a Student Template: Create a blueprint for student information, including their name, age, and the classes they're taking, and then fill in this blueprint with an example student.
+// Filling te blueprint with an example
+var student = {
+    name: "Rafeya Ahsan",
+    age: 19,
+    courses: ["Discrete Mathematics", "Statistical Inference", "Differential Equation"]
+};
+console.log(student);
+// We're using the blueprint to make sure our student has a name, age, and list of courses.
--- Day-21/q62/main.ts
+++ Day-21/q62/main.ts
@@ -1,1 +1,19 @@
+// Question 62: Making a Student Template: Create a blueprint for student information, including their name, age, and the classes they're taking, and then fill in this blueprint with an example student.
 
+// BLUEPRINT: Its called an interfce that provides a structure.
+
+// Creating a blueprint (interface) for student information
+interface Student{
+    name:string ;
+    age:number  ;
+    courses:string[] ;
+}
+// Filling te blueprint with an example
+let student:Student={
+    name :"Rafeya Ahsan",
+    age : 19,
+    courses:["Discrete Mathematics" , "Statistical Inference" , "Differential Equation"]
+
+}
+console.log(student);
+// We're using the blueprint to make sure our student has a name, age, and list of courses.
